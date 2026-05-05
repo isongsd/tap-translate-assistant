598a6050ab76c60bd1bb78a54578224ab1f51b8a
isongsd <isongsd@gmail.com>
2026-05-05 20:46:51 +0800

    Fix iOS Chinese text transform build

--- ios/TapTranslateAssistant/ChineseTextConverter.swift
+++ ios/TapTranslateAssistant/ChineseTextConverter.swift
@@ -1,23 +1,32 @@
 import Foundation
 
 enum ChineseTextConverter {
+    private static let traditionalToSimplified = StringTransform(rawValue: "Traditional-Simplified")
+    private static let simplifiedToTraditional = StringTransform(rawValue: "Simplified-Traditional")
+
     static func convert(_ text: String, from source: TranslationLanguage, to target: TranslationLanguage) -> String {
         guard source != target else { return text }
 
         if source == .traditionalChinese, target == .simplifiedChinese {
-            return transform(text, using: kCFStringTransformTraditionalChineseToSimplified)
+            return transform(text, using: traditionalToSimplified)
         }
 
         if source == .simplifiedChinese, target == .traditionalChinese {
-            return transform(text, using: kCFStringTransformSimplifiedChineseToTraditional)
+            return transform(text, using: simplifiedToTraditional)
         }
 
         return text
     }
 
-    private static func transform(_ text: String, using transform: CFString) -> String {
+    private static func transform(_ text: String, using transform: StringTransform) -> String {
         let mutableText = NSMutableString(string: text)
-        CFStringTransform(mutableText as CFMutableString, nil, transform, false)
-        return mutableText as String
+        let fullRange = NSRange(location: 0, length: mutableText.length)
+        let didTransform = mutableText.applyTransform(
+            transform,
+            reverse: false,
+            range: fullRange,
+            updatedRange: nil
+        )
+        return didTransform ? mutableText as String : text
     }
 }
